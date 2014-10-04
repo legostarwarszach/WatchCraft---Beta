218ee21360336eabf0e6afbc560544d9aa9f1c2b
Legostarwarszach <zfarley2123@gmail.com>
2014-10-04 11:28:47 -0400

    Fixed import probs

--- src/com/zafcoding/android/watchcraft/UrlReader.java
+++ src/com/zafcoding/android/watchcraft/UrlReader.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 
+
 public class UrlReader {
 
 	public ArrayList<String> getFromUrl(URL url) throws IOException{
